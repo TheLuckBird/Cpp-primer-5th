412bafa75833d791f00581b745c6c02041112f58
pfh123456 <2718261451@qq.com>
2023-10-05 21:30:55 +0800

    20231005
    😶

--- chapter15/Quote.h
+++ chapter15/Quote.h
@@ -9,10 +9,12 @@ class Quote
 public:
     Quote() = default;
     Quote(string bookNo_,double price_):bookNo(bookNo_),price(price_){}
+    Quote(const Quote &q):bookNo(q.bookNo),price(q.price){}
     virtual ~Quote() = default;
     
     string isbn() const{return bookNo;}
     virtual double net_price(size_t n) const{return n*price;};
+    virtual void debug() const;
 
 private:
     string bookNo;
@@ -24,9 +26,10 @@ class Bulk_quote:public Quote
 {
 public:
     Bulk_quote() = default;
-    Bulk_quote(string &bookNo_,double price_,size_t n,double disc):Quote(bookNo_,price_),min_qty(n),discount(disc) {}
-
+    Bulk_quote(const string &bookNo_,double price_,size_t n,double disc):Quote(bookNo_,price_),min_qty(n),discount(disc) {}
+    Bulk_quote(const Bulk_quote &b):Quote(b),min_qty(b.min_qty),discount(b.discount){}
     double net_price(size_t)const override ;
+    void debug() const override;
 private:
     size_t min_qty = 0;
     //适用折扣政策的最低购买量
@@ -47,4 +50,15 @@ double print(ostream &os, const Quote &item,size_t n)
     double ret = item.net_price(n);
     cout << item.isbn() << n << "本书利润：" << ret << endl;
     return ret;
+}
+
+void Quote::debug() const
+{
+    cout << "bookNo:" << bookNo << " price:" << price << endl;
+}
+
+void Bulk_quote::debug() const
+{
+    Quote::debug();
+    cout << "min_qty:" << min_qty <<" discount:" << discount << endl;
 }
--- chapter15/part15.1.cpp
+++ chapter15/part15.1.cpp
@@ -5,19 +5,37 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    string s;
-    double price;
-    cout << "请输入书名，价格：" << endl;
-    cin >> s >> price;
-    size_t n;
-    cout << "请输入售卖数量：" << endl;
-    cin >> n;
-    const Quote a(s,price);
-    Bulk_quote b(s,price,10,0.5);
+    // string s;
+    // double price;
+    // cout << "请输入书名，价格：" << endl;
+    // cin >> s >> price;
+    // size_t n;
+    // cout << "请输入售卖数量：" << endl;
+    // cin >> n;
+    // Quote a(s,price);
+    // Bulk_quote b(s,price,10,0.5);
+    // // Bulk_quote b(a);
+    // Quote c(b);
 
-    // cout << a.isbn() << n << "本书利润：" << a.net_price(n) ;
-    print(cout,a,n);
-    print(cout,b,40);
-    
+    // // cout << a.isbn() << n << "本书利润：" << a.net_price(n) ;
+    // print(cout,a,n);
+    // print(cout,b,40);
+
+    // Quote a("cpp primer",10);
+    // Bulk_quote b("data structure",20,10,0.5);
+    // Quote c(b);
+    // cout << c.isbn() << endl;
+    // Bulk_quote d(b);
+    // cout << d.isbn() << endl;
+
+    Quote a("cpp primer",10);
+    Bulk_quote b("data structure",20,10,0.5);
+    // a.debug();
+    // b.debug();
+
+    Quote &x = b;
+    x.debug();
+    Quote *y = &b;
+    y->debug();
     return 0;
 }
