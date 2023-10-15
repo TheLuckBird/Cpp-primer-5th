bd9602c10a0fc4780a56bd322e4ba23ec87d09b7
pfh123456 <2718261451@qq.com>
2023-10-15 21:51:23 +0800

    20231015

--- data structure/List/Main.cpp
+++ data structure/List/Main.cpp
@@ -1,5 +1,6 @@
 #include "Sequence_list.h"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -7,8 +8,17 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     // cout << "hi" << endl;
-    Sequence_list<int> array(4);
+    Sequence_list<int> array(100);
     array.init();
+    array.show();
+    // cout << array.getItemIndex(4) << endl;
+    // string s = "c";
+    // cout << array.getItem(4) << endl;
+
+    // vector<int> ivec;
+    // array.insert(0,999);
+    // array.show();
+    array.deleteItem(4);
     array.show();
     return 0;
 }
--- data structure/List/Sequence_list.h
+++ data structure/List/Sequence_list.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
 using namespace std;
 
@@ -9,9 +11,12 @@ class Sequence_list
 {
 public:
     Sequence_list(size_t size):maxsize(size){data = new T[maxsize];};// T data[maxsize];}
-    void init();
-    void show();
-    // void insert(size_t,T);
+    void init();//初始化线性表
+    void show();//打印线性表
+    size_t getItemIndex(T);//按元素取位置
+    T getItem(size_t);//按位置取元素
+    void insert(size_t,T);
+    void deleteItem(T);
     ~Sequence_list(){delete [] data;}
 
 private:
@@ -23,6 +28,7 @@ private:
 
 template<typename T> void Sequence_list<T>::init()
 {
+    cout << "输入元素:" << endl;
     T v;
     for(size_t i=0;(cin >> v)&&(i!=maxsize);++i,++length)
     {
@@ -33,7 +39,44 @@ template<typename T> void Sequence_list<T>::init()
 
 template<typename T> void Sequence_list<T>::show()
 {
+    cout << "线性表：" << endl;
     for(size_t i=0;i!=length;i++)
         cout << data[i] << " ";
     cout << endl;
+}
+
+template <typename T> size_t Sequence_list<T>::getItemIndex(T v)
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        if(data[i] == v) return i;
+    }
+    throw runtime_error("没找到");
+    
+}
+
+template <typename T> T Sequence_list<T>::getItem(size_t i)
+{
+    if(i < 0 || i>= length) throw runtime_error("越界");
+    else return data[i];
+}
+
+template <typename T> void Sequence_list<T>::insert(size_t p,T v)
+{
+    if(p < 0 || p>= length) throw runtime_error("越界");
+    
+    for(size_t i=length++;i!=p-1;--i)
+        data[i+1] = data[i];
+    data[p] = v;
+    
+}
+
+template <typename T> void Sequence_list<T>::deleteItem(T v)
+{
+    size_t c = 0;//v出现的次数
+    size_t gap = 0;
+    for(size_t i=0;i!=length;++i)
+        if(data[i] == v) ++gap,++c;
+        else data[i-gap] = data[i],gap=0;
+    length = length -c + 1;
 }
